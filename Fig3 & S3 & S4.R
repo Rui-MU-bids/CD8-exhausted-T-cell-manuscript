73b21791399dabf2b8296a5ff31b3ea40bc3ea71
Rui Mu <120769154+Rui-MU-bids@users.noreply.github.com>
2025-06-11 23:58:41 -0500

    Update and rename Fig3 & S2.R to Fig3 & S3 & S4.R

--- Fig3 & S3 & S4.R
+++ Fig3 & S3 & S4.R
@@ -1,9 +1,7 @@
 library(Seurat)
 library(ggplot2)
-install.packages('writexl')
 library(writexl)
 library(dplyr)
-install.packages("clustree")
 library(clustree)
 
 exhaustedT <- subset(Tcells, subset = Cd3e > 0 & Cd8a > 0 & Pdcd1 > 0 & Mki67 == 0 & Cd4 == 0, slot = 'counts')
@@ -28,29 +26,14 @@ Idents(exhaustedT) <- exhaustedT$SCT_snn_res.0.8
 exhaustedT <- RunUMAP(exhaustedT, dims = 1:30, reduction = 'harmony')
 DimPlot(exhaustedT, label = TRUE)
 
-
-exhaustedT <- PrepSCTFindMarkers(exhaustedT)
-exhaustedT.markers <- FindAllMarkers(exhaustedT, only.pos = T, min.pct = 0.1, logfc.threshold = 0.1, recorrect_umi = FALSE)
-exhaustedT.markers %>%
-  group_by(cluster) %>%
-  slice_max(n = 2, order_by = avg_log2FC)
-write_xlsx(exhaustedT.markers,"mice_exhaustedT_markers.xlsx")
-
-#Fig3F
-Idents(exhaustedT) <- exhaustedT$cancertype
-Idents(exhaustedT) <- factor(Idents(exhaustedT),levels = c("FMC","Skin","Pancreatic","Colorectal"))
-plot <- DimPlot(exhaustedT,
-                reduction = "umap",
-                combine = TRUE, 
-                label = F,
-                pt.size = 0.3,
-                cols = c("#D53E4F","#75AADB","#FC8D59","#FEE08B"))
-ggsave("miceCancers_exh.pdf", plot = plot, width = 6, height = 5, dpi = 300)
-
-
-
 Idents(exhaustedT) <- exhaustedT$SCT_snn_res.0.8
 exhaustedT <- subset(exhaustedT, idents = c("0","1","2","3","4","5","6","7","8")) #NK doublets cluster removed
+
+mice_inte_exhaustedT <- FindNeighbors(mice_inte_exhaustedT, dims = 1:30, reduction = 'harmony')
+mice_inte_exhaustedT <- FindClusters(mice_inte_exhaustedT, resolution = c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0)) #FigS3D
+clustree(mice_inte_exhaustedT, prefix = "SCT_snn_res.")
+
+Idents(exhaustedT) <- exhaustedT$SCT_snn_res.0.8
 
 rename<- c("1","4")
 exhaustedT@meta.data$humanC<- ifelse(exhaustedT@meta.data$SCT_snn_res.0.8 %in% rename, "C3", NA)
@@ -86,49 +69,12 @@ VlnPlot(C5,c("Tcf7","Sell"),)
 FeaturePlot(C5, c("Tcf7","Sell")) 
 
 
-#Fig3E
-Idents(exhaustedT) <- factor(Idents(exhaustedT),levels = c("C1","C2","C3","C5","C6"))
-plot <- DimPlot(
-  exhaustedT,
-  combine = TRUE, 
-  label = F,
-  pt.size = 0.3,
-  cols = c("#E69F00", "#56B4E9", "#009E73", "#0072B2","#C26841")
-)
-
-ggsave("miceLabeled_exh.pdf", plot = plot, width = 6, height = 5, dpi = 300)
-
-
-#FigS2A-C
-p1 <- FeaturePlot(exhaustedT, 'Cd3e', slot = 'data', cols = c('lightblue','steelblue'), min.cutoff = 1)
-p2 <- FeaturePlot(exhaustedT, 'Cd8a', slot = 'data', cols = c('lightblue','steelblue'), min.cutoff = 1)
-p3 <- FeaturePlot(exhaustedT, 'Pdcd1', slot = 'data',cols = c('lightblue','steelblue'), min.cutoff = 1)
-ggsave(filename = "Cd8a.pdf", plot = p2, device = "pdf", width = 8, height = 6) 
-
-
-table(exhaustedT$humanC_adj, exhaustedT$cancertype)
-
-#Fig3F
-data <- readxl::read_excel("Cell proportion.xlsx")
-data$Cluster <- factor(data$Cluster,levels = c("C1","C2","C3","C5","C6"))
-data$Cancer <- factor(data$Cancer,levels = c("Brain tumor","Skin cancer","Pancreatic cancer","Colorectal cancer"))
-p1 <- ggplot(data, aes(fill= Cancer, y=Count, x=Cluster)) + 
-  geom_bar(position="fill", stat="identity") + 
-  scale_fill_manual(values = c("#D53E4F","#75AADB","#FC8D59","#FEE08B"))+
-  theme(panel.background = element_rect(fill = "white"))
-ggsave(filename = "human_Cbar.pdf", plot = p1, device = "pdf", width = 8, height = 6) 
-
-p2 <- ggplot(data, aes(fill = Cluster, y = Count, x = Cancer)) + 
-  geom_bar(position = "fill", stat = "identity") + 
-  scale_fill_manual(values = c("#E69F00", "#56B4E9", "#009E73", "#0072B2","#C26841")) +
-  theme(
-    panel.background = element_rect(fill = "white"),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  )
-ggsave(filename = "cancer_Cbar.pdf", plot = p2, device = "pdf", width = 8, height = 6)  
-
-
-saveRDS(exhaustedT, "mice_exhaustedT.RDS")
+exhaustedT <- PrepSCTFindMarkers(exhaustedT)
+exhaustedT.markers <- FindAllMarkers(exhaustedT, only.pos = T, min.pct = 0.1, logfc.threshold = 0.1, recorrect_umi = FALSE)
+exhaustedT.markers %>%
+  group_by(cluster) %>%
+  slice_max(n = 2, order_by = avg_log2FC)
+write_xlsx(exhaustedT.markers,"mice_exhaustedT_markers.xlsx")
 
 
 
@@ -142,7 +88,6 @@ line_size <- 1
 base_size <- 12
 axis_text_rel_size <- -1
 title_text_rel_size <- 2
-
 
 plot <- ggplot(data, aes(x = Cancer, y = `CD8 exhausted T cell percentage`)) +
   stat_summary(fun = mean, geom = "bar", fill = "white",color = "black", width = 0.7) +
@@ -168,6 +113,52 @@ plot <- ggplot(data, aes(x = Cancer, y = `CD8 exhausted T cell percentage`)) +
     plot.background = element_blank()
   )
 ggsave(filename = "percentage_mice.pdf", plot = plot, device = "pdf", width = 8, height = 6)  
+
+
+
+#Fig3C
+Idents(exhaustedT) <- factor(Idents(exhaustedT),levels = c("C1","C2","C3","C5","C6"))
+plot <- DimPlot(
+  exhaustedT,
+  combine = TRUE, 
+  label = F,
+  pt.size = 0.3,
+  cols = c("#E69F00", "#56B4E9", "#009E73", "#0072B2","#C26841")
+)
+
+ggsave("miceLabeled_exh.pdf", plot = plot, width = 6, height = 5, dpi = 300)
+
+
+#Fig3D
+Idents(exhaustedT) <- exhaustedT$cancertype
+Idents(exhaustedT) <- factor(Idents(exhaustedT),levels = c("FMC","Skin","Pancreatic","Colorectal"))
+plot <- DimPlot(exhaustedT,
+                reduction = "umap",
+                combine = TRUE, 
+                label = F,
+                pt.size = 0.3,
+                cols = c("#D53E4F","#75AADB","#FC8D59","#FEE08B"))
+ggsave("miceCancers_exh.pdf", plot = plot, width = 6, height = 5, dpi = 300)
+
+
+#Fig3E-F
+data <- readxl::read_excel("Cell proportion.xlsx")
+data$Cluster <- factor(data$Cluster,levels = c("C1","C2","C3","C5","C6"))
+data$Cancer <- factor(data$Cancer,levels = c("Brain tumor","Skin cancer","Pancreatic cancer","Colorectal cancer"))
+p1 <- ggplot(data, aes(fill= Cancer, y=Count, x=Cluster)) + 
+  geom_bar(position="fill", stat="identity") + 
+  scale_fill_manual(values = c("#D53E4F","#75AADB","#FC8D59","#FEE08B"))+
+  theme(panel.background = element_rect(fill = "white"))
+ggsave(filename = "Cbar.pdf", plot = p1, device = "pdf", width = 8, height = 6) 
+
+p2 <- ggplot(data, aes(fill = Cluster, y = Count, x = Cancer)) + 
+  geom_bar(position = "fill", stat = "identity") + 
+  scale_fill_manual(values = c("#E69F00", "#56B4E9", "#009E73", "#0072B2","#C26841")) +
+  theme(
+    panel.background = element_rect(fill = "white"),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+ggsave(filename = "cancer_Cbar.pdf", plot = p2, device = "pdf", width = 8, height = 6)  
 
 
 #Fig3G
@@ -198,12 +189,21 @@ ggsave(filename = "pathway.pdf", plot = p1, device = "pdf", width = 10, height =
 
 
 
-#FigS2F
-subtypes <- c('Ccl3',"Ccl4","Klrd1","Ifng",
-              "S100a4","S100a6","S100a13",
-              "Il7r","Tcf7","Ccr7","Sell",
-              "Ifit1","Ifit2","Ifit3","Oas3","Isg15",
-              "Cdca8","Birc5","Cdc45","Mcm3")
+
+#FigS3A-C
+p1 <- FeaturePlot(exhaustedT, 'Cd3e', slot = 'data', cols = c("lavender", "#bd7fef"), min.cutoff = 1)
+p2 <- FeaturePlot(exhaustedT, 'Cd8a', slot = 'data', cols = c("lavender", "#bd7fef"), min.cutoff = 1)
+p3 <- FeaturePlot(exhaustedT, 'Pdcd1', slot = 'data',cols = c("lavender", "#bd7fef"), min.cutoff = 1)
+ggsave(filename = "Pdcd1.pdf", plot = p3, device = "pdf", width = 8, height = 6)
+
+
+
+#FigS4C
+subtypes <- c('Ccl3',"Ccl4","Ccl5","Klrd1","Ifng","Cd200r4",
+              "S100a4","S100a6","S100a13","Lgals1","Lgals3",
+              "Il7r","Tcf7","Ccr7","Ccr8","Sell","Cd83","Il2ra","Slamf6",
+              "Ifit1","Ifit2","Ifit3","Ifi208","Ifit3b","Oas3","Oasl2","Isg15","Stat2","Rsad2",
+              "Cdca8","Birc5","Cdc45","Mcm3","Cdk1","Tacc3","Gins2","Cks1b","Rad51")
 DefaultAssay(exhaustedT) <- 'SCT'
 avg_exp <- AverageExpression(exhaustedT, assays = "SCT", features = subtypes, group.by = 'humanC', return.seurat = T)
 my_colors <- rev(brewer.pal(n = 7, name = "RdBu"))
@@ -221,13 +221,11 @@ DoHeatmap(
     axis.text.x = element_text(angle = 45, hjust = 1)  
   )
 
-#FigS2D,G,H
+#FigS4D-E
 Idents(exhaustedT) <- factor(Idents(exhaustedT),levels = c("C1","C2","C3","C5","C6"))
-Exhaustion <- c("Entpd1","Havcr2","Ctla4","Lag3","Pdcd1","Tox","Tigit")
 effect <- c("Runx3","Klrg1","Fas","Spn","Cd44","Osr2","Cxcr1")
 others <- c("Cxcr5","Slamf6","Prdm1","Cd69","Cd101")
-plot <- DotPlot(exhaustedT, features = Exhaustion, dot.scale = 10, scale = T, cols = c("lightgrey", "#bd7fef"),scale.min = 0, scale.max = 100)+coord_flip()
-plot <- DotPlot(exhaustedT, features = others, dot.scale = 10, scale = T, cols = c("lightgrey", "#bd7fef"))+coord_flip()
 plot <- DotPlot(exhaustedT, features = effect, dot.scale = 10, scale = F, cols = c("lightgrey", "#bd7fef"),scale.min = 0, scale.max = 100)+coord_flip()+scale_color_gradientn(colors = c("lightgrey", "#bd7fef"), limits = c(0, 5))
+plot <- DotPlot(exhaustedT, features = others, dot.scale = 10, scale = T, cols = c("lightgrey", "#bd7fef"))+coord_flip()
 ggsave(filename = "mice_eff.pdf", plot = plot, device = "pdf", width = 7, height = 5) 
 
